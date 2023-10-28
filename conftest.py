889b9296b8e79418add423a2e64cf808048fc92b
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-10-28 19:13:22 +0300

    added dotenv

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from selenium import webdriver
@@ -7,6 +9,12 @@ from selenium.webdriver.chrome.options import Options
 from utils import attach
 from pages.registration_form_page import RegistrationFormPage
 
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
@@ -23,8 +31,11 @@ def setup_browser():
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
